9219638a730492f956b3f8a63a3ac6e58442f9fb
sheikh umaid <73013686+SheikhUmaid@users.noreply.github.com>
2023-02-24 15:29:28 +0530

    serve

--- django_project/urls.py
+++ django_project/urls.py
@@ -16,8 +16,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
+from django.views.static import serve
 from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include("Home.urls"))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) 
+
+if settings.DEBUG == False:
+    urlpatterns += [path('media/<path:path>', serve, {'document_root': settings.MEDIA_ROOT,}),]
+
+
